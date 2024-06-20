8c584ed4fd4748c0da3d9da067edb3652b082c3c
mudassirh45 <mudassirh45@gmail.com>
2024-06-20 14:31:24 +0500

    [Add] mh_hide_powered_by_odoo_website module

---
+++ mh_hide_powered_by_odoo_website/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Hide Powered By Odoo',
+    'summary': """Hide Powered By Odoo and Manage Database""",
+    'sequence': -1,
+    'version': '17.0.1.0.0',
+    'category': 'Website',
+    'author': 'Mudassir Hassan',
+    'website': 'http://mudassirh45@gmail.com',
+    'module_type': 'industries',
+    'price': '6.0',
+    'currency': 'USD',
+    'depends': ['base', 'web','website'],
+    'data': [
+        'views/views.xml',
+        'views/custom_template.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [
+        ],
+        'web.assets_backend': [
+        ],
+    },
+    'license': 'OPL-1',
+}
