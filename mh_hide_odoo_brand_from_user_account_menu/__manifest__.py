8e433935c68e7a261d661afe3d22ee06399810d5
mudassirh45 <mudassirh45@gmail.com>
2024-07-01 10:28:45 +0500

    [Add] mh_hide_odoo_brand_from_user_account_menu for odoo15

---
+++ mh_hide_odoo_brand_from_user_account_menu/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Hide Documentation,Support,Shortcut Menu',
+    'summary': """Hide Documentation,Support,Shortcut Menu""",
+    'sequence': -1,
+    'version': '15.0.1.0.0',
+    'category': 'Website',
+    'author': 'Mudassir Hassan',
+    'website': 'http://mudassirh45@gmail.com',
+    'module_type': 'industries',
+    'price': '4.60',
+    'currency': 'USD',
+    'depends': ['base','web'],
+    'data': [
+        'views/views.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [
+        ],
+        'web.assets_backend': [
+            'mh_hide_odoo_brand_from_user_account_menu/static/src/js/remove_specific_menu.js',
+        ],
+    },
+    'license': 'OPL-1',
+}
