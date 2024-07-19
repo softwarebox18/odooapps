3db4d09f575b0459986615a3fc8f7fa652233f65
mudassirh45 <mudassirh45@gmail.com>
2024-07-19 11:30:59 +0500

    [Add] mh_dynamic_many2one_field_options odoo16

---
+++ mh_dynamic_many2one_field_options/__manifest__.py
@@ -0,0 +1,27 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Dynamic Many2One Field Options',
+    'summary': """Dynamic Many2One Field Options""",
+    'sequence': -1,
+    'version': '16.0.1.0.0',
+    'category': 'Website',
+    'author': 'Mudassir Hassan',
+    'website': 'http://mudassirh45@gmail.com',
+    'module_type': 'industries',
+    'price': '44.32',
+    'currency': 'USD',
+    'depends': ['base', 'web'],
+    'data': [
+        'views/views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_backend': [
+            'mh_dynamic_many2one_field_options/static/src/js/dynamic_many2one_field_options.js',
+        ],
+    },
+    'license': 'OPL-1',
+}
---
+++ mh_dynamic_many2one_field_options/static/src/js/dynamic_many2one_field_options.js
@@ -0,0 +1,82 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { Many2OneField } from "@web/views/fields/many2one/many2one_field";
+import { useService } from "@web/core/utils/hooks";
+const { useRef, onPatched, onMounted, useState } = owl;
+
+patch(Many2OneField.prototype, 'dynamic_many2one_field_options_patch', {
+    setup() {
+        this._super.apply(this, arguments);
+        this.orm = useService("orm");
+        this.state = useState({
+            many2OneConfigDict: {},
+        });
+
+        // Ensure activeActions and other props are initialized
+        this.activeActions = this.activeActions || {};
+        this.many2oneProps = this.many2oneProps || {};
+
+        // Fetch configuration on component mount
+        onMounted(() => {
+            this.fetchConfig();
+        });
+    },
+
+    async fetchConfig() {
+        const currentModel = this.props.record.resModel;
+        const result = await this.orm.searchRead(
+            "many2one.field.config",
+            [["model_id.model", "=", currentModel]],
+            ["config_line_ids"]
+        );
+        if (result.length > 0) {
+            const configId = result[0].id;
+            const configLines = await this.orm.searchRead(
+                "many2one.field.config.lines",
+                [["config_id", "=", configId]],
+                ["fields_id", "is_hide_create_edit", "is_hide_search_more", "search_limit", "quick_search_more_limit", "can_open"]
+            );
+            const many2OneConfigDictInner = {};
+            for (const line of configLines) {
+                const field = await this.orm.read(
+                    "ir.model.fields",
+                    [line.fields_id[0]],
+                    ["name"]
+                );
+                many2OneConfigDictInner[field[0].name] = {
+                    'is_hide_create_edit': line.is_hide_create_edit,
+                    'search_limit': line.search_limit,
+                    'quick_search_more_limit': line.quick_search_more_limit,
+                    'is_hide_search_more': line.is_hide_search_more,
+                    'can_open': line.can_open,
+                };
+            }
+
+            this.state.many2OneConfigDict = await many2OneConfigDictInner;
+        }
+    },
+
+    get Many2XAutocompleteProps() {
+        const fieldName = this.props.name;
+        const fieldConfig = this.state.many2OneConfigDict[fieldName] || { is_hide_create_edit: false, search_limit: 0, is_hide_search_more: false };
+
+        const is_hide_create_edit =  fieldConfig.is_hide_create_edit ? null : function() {};
+        const search_limit =  fieldConfig.search_limit > 0 ? fieldConfig.search_limit - 1 : 7;
+        const quick_search_more_limit =  fieldConfig.quick_search_more_limit > 0 ? fieldConfig.quick_search_more_limit : 320;
+        const is_hide_search_more =  fieldConfig.is_hide_search_more ? fieldConfig.is_hide_search_more : false;
+        const canOpen =  fieldConfig.can_open ? fieldConfig.can_open : false;
+        this.props.canOpen = canOpen
+
+        return {
+            ...this._super.apply(this, arguments),
+            quickCreate: is_hide_create_edit,
+            searchLimit: search_limit,
+            searchMoreLimit: quick_search_more_limit,
+            noSearchMore: is_hide_search_more,
+            activeActions: this.activeActions,
+
+        };
+    },
+});
+
