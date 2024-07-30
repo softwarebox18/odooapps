70f0ceaaa4d9f247b7bbda160cb322de81297618
mudassirh45 <mudassirh45@gmail.com>
2024-07-30 11:11:07 +0500

    [Add] mh_product_and_qty_details_in_sale_order odoo15

---
+++ mh_product_and_qty_details_in_sale_order/__manifest__.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Display Order Details in Sale Order List View',
+    'summary': """Display Order Details in Sale Order List View""",
+    'sequence': -1,
+    'version': '15.0.1.0.0',
+    'category': 'Sale',
+    'author': 'Software Box',
+    'website': 'http://softwarebox18@gmail.com',
+    'price': '3.31',
+    'currency': 'USD',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/views.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1',
+}
