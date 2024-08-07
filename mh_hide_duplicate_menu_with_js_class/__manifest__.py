fd135b92f257046db75305cf66d6e2e3139aa782
mudassirh45 <mudassirh45@gmail.com>
2024-08-07 15:58:42 +0500

    [Add] mh_hide_duplicate_menu_with_js_class cp odoo17

--- mh_hide_duplicate_menu_with_js_class/__manifest__.py
+++ mh_hide_duplicate_menu_with_js_class/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Software Box',
     'website': 'http://softwarebox18@gmail.com',
     'module_type': 'industries',
-    'price': '16.83',
+    'price': '7.83',
     'currency': 'USD',
     'depends': ['base', 'web', 'sale'],
     'data': [
