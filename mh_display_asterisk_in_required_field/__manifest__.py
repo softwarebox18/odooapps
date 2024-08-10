9fb4843d358d3ffceea20befc64be537674d98f3
mudassirh45 <mudassirh45@gmail.com>
2024-08-10 15:30:18 +0500

    [Add] mh_display_asterisk_in_required_field odoo17

---
+++ mh_display_asterisk_in_required_field/__manifest__.py
@@ -0,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Display Asterisk in Required Fields',
+    'summary': """Display Asterisk in Required Fields""",
+    'sequence': -1,
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'author': 'Software Box',
+    'website': 'http://softwarebox18@gmail.com',
+    'price': '1.00',
+    'currency': 'USD',
+    'depends': ['base','web'],
+    'data': [
+        'views/views.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [
+        ],
+        'web.assets_backend': [
+            'mh_display_asterisk_in_required_field/static/src/views/**/*',
+        ],
+    },
+    'license': 'OPL-1',
+}
