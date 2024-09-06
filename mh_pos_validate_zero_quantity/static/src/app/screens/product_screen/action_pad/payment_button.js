b047f409128d0fe55f69e792483117a922f5cf3c
mudassirh45 <mudassirh45@gmail.com>
2024-09-06 10:31:04 +0500

    [Add] mh_pos_validate_zero_quantity odoo15

---
+++ mh_pos_validate_zero_quantity/static/src/app/screens/product_screen/action_pad/payment_button.js
@@ -0,0 +1,36 @@
+/** @odoo-module */
+
+import ProductScreen from 'point_of_sale.ProductScreen';
+import { patch } from 'web.utils';
+
+// Patch the Order model to override the pay() method
+patch(ProductScreen.prototype, "mh_pos_validate_zero_quantity.validate_zero_quantity_pos_order", {
+    async _onClickPay() {
+        // Add your custom logic before calling the original function
+        const order = this.env.pos.get_order();
+        // Check if there are no order lines (empty order)
+        if (order.orderlines.length === 0) {
+            await this.showPopup('ErrorPopup', {
+                title: this.env._t("Empty Order"),
+                body: this.env._t("There are no products in the order. Please add products to proceed."),
+            });
+            return;  // Stop further execution if no products in the order
+        }
+
+        // Check if any orderlines have zero quantity
+        const zeroQuantityLines = order.orderlines.filter(line => line.quantity === 0);
+        if (zeroQuantityLines.length > 0) {
+            const productNames = zeroQuantityLines.map((line, index) => (index + 1) + ". " + line.product.display_name).join("\n");
+            await this.showPopup('ErrorPopup', {
+                title: this.env._t("Invalid Quantities"),
+                body: this.env._t("The following products have 0 quantity:\n" + productNames + "\n\n Please correct the quantities to proceed."),
+                body_html: true, // Enable HTML for line breaks
+            });
+            return;  // Stop further execution if validation fails
+        }
+
+        // Call the original _onClickPay method
+        await this._super.apply(this, arguments);
+    }
+});
+
