f74ca1faf7de0251c44e39f707930965b81fc805
mudassirh45 <mudassirh45@gmail.com>
2024-10-16 14:57:19 +0500

    [Add] mh_all_company_select_unselect odoo18

---
+++ mh_all_company_select_unselect/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Select/Unselect All Companies from Multi-company',
+    'summary': """Select/Unselect All Companies from Multi-company""",
+    'sequence': -1,
+    'version': '18.0.1.0.0',
+    'category': 'Website',
+    'author': 'Software Box',
+    'website': 'http://softwarebox18@gmail.com',
+    'module_type': 'industries',
+    'price': '7.90',
+    'currency': 'USD',
+    'depends': ['base','web'],
+    'data': [
+        'views/views.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main.PNG'],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [
+        ],
+        'web.assets_backend': [
+            'mh_all_company_select_unselect/static/src/webclient/**/*',
+        ],
+    },
+    'license': 'OPL-1',
+}
